8b2ea0f6634e6a2df72f44c77db95ea47e4aa54d
Jhon Arley Castañeda Vergara <arleycastaneda22@gmail.com>
2023-11-10 21:17:48 -0500

    Organización de las carpetas

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,5 @@
 import mongoose from 'mongoose';
 
-// try {
-//     await mongoose.connect(process.env.URI_MONGO)
-//         console.log('Database is Connected');
-// } catch (error) {
-//     console.log('Error de conexión a la base de datos:'+ error)
-// }
-
 
 mongoose.connect(process.env.URI_MONGO)
   .then(db => console.log('Database is Connected'))
