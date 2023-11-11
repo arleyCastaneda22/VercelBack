da6a26fa81ca1af8854b5c1ac4fdf9fe8372b146
Jhon Arley Castañeda Vergara <arleycastaneda22@gmail.com>
2023-11-10 21:06:40 -0500

    Configurando variables de entorno

--- src/database/database.js
+++ src/database/database.js
@@ -8,6 +8,6 @@ import mongoose from 'mongoose';
 // }
 
 
-mongoose.connect("mongodb+srv://fernando:1001@cluster0.riw8wx4.mongodb.net/BeautySoft")
+mongoose.connect(process.env.URI_MONGO)
   .then(db => console.log('Database is Connected'))
   .catch(err => console.error('Error de conexión a la base de datos:', err));
