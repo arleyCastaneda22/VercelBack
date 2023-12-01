9bb9c79ca781504427fbea0300081db4b1428aa7
Fernando Rojas <juanrojas.jfrc@gmail.com>
2023-12-01 16:08:38 -0500

    listado de usuarios con su rol completo

--- src/controllers/Usuario.controller.js
+++ src/controllers/Usuario.controller.js
@@ -3,7 +3,7 @@ import User from '../models/User.js'
 
 export const listarUsuarios = async(req, res)=>{
     try {
-        const user= await User.find();
+        const user= await User.find().populate('roles','nombre');
         res.status(200).json(user)
     } catch (error) {
         console.log(error)
