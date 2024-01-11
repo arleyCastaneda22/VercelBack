de36aaef731b67536615113b31febd8225982dc7
Fernando Rojas <juanrojas.jfrc@gmail.com>
2024-01-11 13:34:20 -0500

    estructura de cita

--- src/models/Cita.js
+++ src/models/Cita.js
@@ -1,5 +1,12 @@
+import mongoose from 'mongoose';
 import { Schema, model } from "mongoose";
 
 const citaSchema =new Schema({
-    
+    fecha: Date,
+    estilista: String,
+    servicio: String,
 })
+
+const Cita = mongoose.model('Cita', citaSchema);
+
+module.exports = Cita
