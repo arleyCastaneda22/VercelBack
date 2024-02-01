257eced743e2f9991a9f47c754a73c061664a0c2
Jhon Arley Castañeda Vergara <arleycastaneda22@gmail.com>
2024-02-01 09:41:20 -0500

    trabajando en citas

--- src/controllers/Cita.controller.js
+++ src/controllers/Cita.controller.js
@@ -1,8 +1,6 @@
 
 import Cita from '../models/Cita.js';
 import Turno from '../models/Turnos.js';
-
-
 export const createCita = async (req, res) => {
   try {
     const { cliente, servicio, estilista, fechaCita, horaCita } = req.body;
@@ -13,6 +11,16 @@ export const createCita = async (req, res) => {
     fechaCitaNormalizada.setMilliseconds(0);
     horaCitaNormalizada.setMilliseconds(0);
 
+    const duracionCita = 60 * 60 * 1000; // Duración en milisegundos (1 hora)
+    const horaFinCitaNormalizada = new Date(horaCitaNormalizada.getTime() + duracionCita);
+
+    // Ajustar las fechas a la precisión de minutos
+    fechaCitaNormalizada.setSeconds(0, 0);
+    horaCitaNormalizada.setSeconds(0, 0);
+    horaFinCitaNormalizada.setSeconds(0, 0);
+
+
+
     const diaSemana = obtenerDiaSemana(fechaCitaNormalizada.getDay());
     const turno = await Turno.findOne({ estilista, dia: diaSemana });
 
@@ -22,30 +30,41 @@ export const createCita = async (req, res) => {
 
     const { inicioM, finM, inicioT, finT } = turno;
 
+    // Ajustar las fechas a la precisión de minutos
+    inicioM.setSeconds(0, 0);
+    finM.setSeconds(0, 0);
+    inicioT.setSeconds(0, 0);
+    finT.setSeconds(0, 0);
+
     if (
-      !(horaCitaNormalizada >= new Date(inicioM) && horaCitaNormalizada <= new Date(finM)) &&
-      !(horaCitaNormalizada >= new Date(inicioT) && horaCitaNormalizada <= new Date(finT))
+      !(horaCitaNormalizada >= inicioM && horaFinCitaNormalizada <= finM) &&
+      !(horaCitaNormalizada >= inicioT && horaFinCitaNormalizada <= finT)
     ) {
       return res.status(400).json({ error: 'La hora de la cita está fuera del rango de trabajo del estilista.' });
     }
 
-    // Verificar si ya existe una cita para el estilista en la misma fecha y hora
-    const existingCita = await Cita.findOne({
+    // Verificar si ya existe una cita para el estilista en el mismo rango de horas
+    const existingCitaSameRange = await Cita.findOne({
       estilista,
       fechaCita: fechaCitaNormalizada,
-      horaCita: horaCitaNormalizada,
+      $or: [
+        { $and: [{ horaCita: { $gte: horaCitaNormalizada } }, { horaCita: { $lt: horaFinCitaNormalizada } }] },
+        { $and: [{ horaFinCita: { $gte: horaCitaNormalizada } }, { horaFinCita: { $lt: horaFinCitaNormalizada } }] },
+      ],
     });
 
-    if (existingCita) {
-      return res.status(400).json({ error: 'Ya existe una cita para el estilista en la misma fecha y hora.' });
+    if (existingCitaSameRange) {
+      return res.status(400).json({ error: 'Ya existe una cita para el estilista en el mismo rango de horas.' });
     }
 
+    // Crear y guardar la nueva cita
     const cita = new Cita({
       cliente,
       servicio,
       estilista,
       fechaCita: fechaCitaNormalizada,
       horaCita: horaCitaNormalizada,
+      horaFinCita: horaFinCitaNormalizada,
       turno: turno._id,
     });
 
@@ -59,6 +78,7 @@ export const createCita = async (req, res) => {
         estilista: citaSave.estilista,
         fechaCita: citaSave.fechaCita,
         horaCita: citaSave.horaCita,
+        horaFinCita: citaSave.horaFinCita,
       },
     });
   } catch (error) {
@@ -67,10 +87,12 @@ export const createCita = async (req, res) => {
   }
 };
 
+
 function obtenerDiaSemana(dia) {
   const diasSemana = ['L', 'M', 'X', 'J', 'V', 'S', 'D'];
   return diasSemana[dia];
 }
+
 
 
 
