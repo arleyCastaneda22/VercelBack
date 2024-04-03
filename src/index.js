a35c645bb233a7196ad8af6a514a7fb9b0d1775b
Jhon Arley Castañeda Vergara <arleycastaneda22@gmail.com>
2024-04-02 19:31:26 -0500

    eliminacion de cliente y ventas

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import morgan from "morgan";
 //modulos
 import { createRoles }  from './libs/initialSetup.js'
 import EstilistaRouter from './routes/estilista.routes.js'
-import ClienteRouter from './routes/cliente.routes.js'
 import authRouther from './routes/auth.routes.js'
 import ServicioRouter from './routes/servicio.routes.js'
 import TurnosRouter from './routes/turno.js'
 import UsuarioRouter from './routes/usuario.routes.js'
 import RoleRouter from './routes/role.routes.js'
-import ventasRouter from './routes/ventas.routes.js'
 import CitaRouter from './routes/cita.routes.js'
 import dashboardRouter from './routes/dashboard.routes.js'
 
@@ -35,14 +33,11 @@ app.use('/',authRouther)
 
 app.use('/api', EstilistaRouter)
 
-app.use('/api', ClienteRouter)
-
 app.use('/api', ServicioRouter)
 app.use('/api', UsuarioRouter )
 app.use('/api', TurnosRouter)
 
 app.use('/api',RoleRouter)
-app.use('/api', ventasRouter)
 app.use('/api', CitaRouter)
 app.use('/api', dashboardRouter)
 
